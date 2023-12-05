34a392cf79735c1f450534cbdbe88d5281832ffc
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2023-12-05 21:57:17 +0500

    components and routing

--- erp/src/app/app.module.ts
+++ erp/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { InvoicesComponent } from './components/dashboard/invoices/invoices.component';
+import { ClientsComponent } from './components/dashboard/clients/clients.component';
+import { MainComponent } from './components/dashboard/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    InvoicesComponent,
+    ClientsComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
