0e610ffdc38e68a2264601605cdb0db8d1ccda06
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2023-12-07 21:17:31 +0500

    complete components with routing

--- erp/src/app/components/dashboard/dashboard-routing/dashboard-routing.module.ts
+++ erp/src/app/components/dashboard/dashboard-routing/dashboard-routing.module.ts
@@ -3,14 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { InvoicesComponent } from '../invoices/invoices.component';
 import { MainComponent } from '../main/main.component';
 import { ClientsComponent } from '../clients/clients.component';
-
+import { HomeComponent } from '../home/home.component';
+import { ProductComponent } from '../product/product.component';
+import { ChatComponent } from '../chat/chat.component';
+import { SettingsComponent } from '../settings/settings.component';
+import { InventoryComponent } from '../inventory/inventory.component';
 const routes: Routes = [
   {
     path: 'main',
     component: MainComponent,
     children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
       { path: 'invoices', component: InvoicesComponent },
+      { path: 'inventory', component: InventoryComponent },
       { path: 'clients', component: ClientsComponent },
+      { path: 'product', component: ProductComponent },
+      { path: 'chat', component: ChatComponent },
+      { path: 'settings', component: SettingsComponent },
     ],
   },
 ];
