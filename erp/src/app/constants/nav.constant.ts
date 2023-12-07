709f904bfc24eeaa3c3569d4cec9782c553fb20e
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2023-12-07 21:29:58 +0500

    small bug

--- erp/src/app/constants/nav.constant.ts
+++ erp/src/app/constants/nav.constant.ts
@@ -1,9 +1,9 @@
 export const admin = [
-  { url: '#', icon: 'icon', title: 'Home' },
+  { url: 'home', icon: 'icon', title: 'Home' },
   { url: 'invoices', icon: 'icon', title: 'Invoices' },
-  { url: '#', icon: 'icon', title: 'Inventory' },
+  { url: 'inventory', icon: 'icon', title: 'Inventory' },
+  { url: 'product', icon: 'icon', title: 'Product' },
   { url: 'clients', icon: 'icon', title: 'Clients' },
-  { url: '#', icon: 'icon', title: 'Product' },
-  { url: '#', icon: 'icon', title: 'Chat' },
-  { url: '#', icon: 'icon', title: 'Settings' },
+  { url: 'chat', icon: 'icon', title: 'Chat' },
+  { url: 'settings', icon: 'icon', title: 'Settings' },
 ];
