0dd632b4fa51ab48b495d3f846908e6633b3071b
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2023-12-08 23:58:49 +0500

    signup form

--- erp/src/app/components/signup/signup.component.ts
+++ erp/src/app/components/signup/signup.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent {
-
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {
+    this.signupForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+  onSubmit() {}
 }
