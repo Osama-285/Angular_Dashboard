fb10a3934e9619f4bedaa87407cbdbcb49032ca3
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2023-12-21 23:55:10 +0500

    fix axis

--- erp/src/app/components/dashboard/home/home.component.ts
+++ erp/src/app/components/dashboard/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { axis } from 'src/app/constants/graph.constants';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css', './homeBottom.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  graphAxis = axis;
+}
