07716ac495d6b43d9a88b1c77b86a542e50d94f0
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2023-12-24 23:02:15 +0500

    home screen complete

--- erp/src/app/constants/notification.constants.ts
+++ erp/src/app/constants/notification.constants.ts
@@ -7,7 +7,7 @@ export const notifyData = [
   },
   {
     sender: 'Jim',
-    platform: 'outlook1',
+    platform: 'invoice',
     message:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Essemaiores iusto animi totam debitis impedit error hic reprehenderit,velit ipsa voluptatem minus ipsam?',
   },
@@ -18,8 +18,8 @@ export const notifyData = [
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Essemaiores iusto animi totam debitis impedit error hic reprehenderit,velit ipsa voluptatem minus ipsam?',
   },
   {
-    sender: 'Jim',
-    platform: 'outlook1',
+    sender: 'Tim',
+    platform: 'jira',
     message:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Essemaiores iusto animi totam debitis impedit error hic reprehenderit,velit ipsa voluptatem minus ipsam?',
   },
