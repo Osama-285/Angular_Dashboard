9d31f72651d02d942cecb36ded7cbe443dfde467
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2023-12-25 20:31:02 +0500

    invoices top

--- erp/src/app/components/dashboard/invoices/invoices.component.ts
+++ erp/src/app/components/dashboard/invoices/invoices.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { invoiceInfo } from 'src/app/constants/invoices.constants';
 @Component({
   selector: 'app-invoices',
   templateUrl: './invoices.component.html',
-  styleUrls: ['./invoices.component.css']
+  styleUrls: ['./invoices.component.css'],
 })
 export class InvoicesComponent {
-
+  colors = ['color1', 'color2', 'color3', 'color4'];
+  protected invoices = invoiceInfo;
 }
---
+++ erp/src/app/constants/invoices.constants.ts
@@ -0,0 +1,26 @@
+export const invoiceInfo = [
+  {
+    heading: 'Revenue',
+    icon: 'monetization_on',
+    value: '231k',
+    color: 'color1',
+  },
+  {
+    heading: 'Invoices',
+    icon: 'description',
+    value: '2,221',
+    color: 'color2',
+  },
+  {
+    heading: 'Paid',
+    icon: 'done_all',
+    value: '1,154',
+    color: 'color3',
+  },
+  {
+    heading: 'Due',
+    icon: 'emergency_home',
+    value: '1,067',
+    color: 'color4',
+  },
+];
