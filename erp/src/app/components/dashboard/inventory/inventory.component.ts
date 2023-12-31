ab88a6b18dee71f2ad94c1b5bc536cbdfa9edd39
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-01-01 00:52:49 +0500

    update inventory

--- erp/src/app/components/dashboard/inventory/inventory.component.ts
+++ erp/src/app/components/dashboard/inventory/inventory.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.css']
+  styleUrls: ['./inventory.component.css', './inventoryItem.component.css'],
 })
-export class InventoryComponent {
-
-}
+export class InventoryComponent {}
