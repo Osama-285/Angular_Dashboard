79c13fa427aa117b763133c16b45aad732a6c7e5
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-01-06 23:50:22 +0500

    directive complete

--- erp/src/app/directives/tool-tip.directive.ts
+++ erp/src/app/directives/tool-tip.directive.ts
@@ -10,6 +10,35 @@ import {
   selector: '[appToolTip]',
 })
 export class ToolTipDirective {
-  @Input() appToolTip: any;
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  @Input() appTooltip: any;
+  tooltipElement: any;
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    this.tooltipElement = this.renderer.createElement('div');
+    this.renderer.addClass(this.tooltipElement, 'tooltip');
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    if (this.appTooltip) {
+      this.showTooltip();
+    }
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.hideTooltip();
+  }
+
+  private showTooltip() {
+    const tooltipContent = document.createTextNode(this.appTooltip.name);
+    this.renderer.appendChild(this.tooltipElement, tooltipContent);
+
+    const parentElement = this.el.nativeElement.parentNode;
+    this.renderer.appendChild(parentElement, this.tooltipElement);
+  }
+
+  private hideTooltip() {
+    this.renderer.removeChild(
+      this.el.nativeElement.parentNode,
+      this.tooltipElement
+    );
+  }
 }
