b0cb0a9de532a6bd344f462fa94a806c46eea318
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-01-31 23:49:28 +0500

    adding clients

--- erp/src/app/components/dashboard/clients/clients.component.ts
+++ erp/src/app/components/dashboard/clients/clients.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { clients } from 'src/app/constants/client.constants';
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
-  styleUrls: ['./clients.component.css']
+  styleUrls: ['./clients.component.css'],
 })
 export class ClientsComponent {
-
+  protected clientsInfo = clients;
 }
---
+++ erp/src/app/constants/client.constants.ts
@@ -0,0 +1,38 @@
+export const clients = [
+  {
+    revenue: '22k',
+    invoices: '246',
+    paid: '246',
+    due: '246',
+  },
+  {
+    revenue: '19k',
+    invoices: '26',
+    paid: '46',
+    due: '26',
+  },
+  {
+    revenue: '2k',
+    invoices: '2',
+    paid: '24',
+    due: '6',
+  },
+  {
+    revenue: '22k',
+    invoices: '246',
+    paid: '246',
+    due: '246',
+  },
+  {
+    revenue: '10k',
+    invoices: '19',
+    paid: '6',
+    due: '13',
+  },
+  {
+    revenue: '3k',
+    invoices: '19',
+    paid: '58',
+    due: '29',
+  },
+];
