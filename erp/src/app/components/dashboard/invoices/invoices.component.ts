d41ebe06b26ae56a0b9ee8e2d230aaf8b55a5aff
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-02-02 23:51:26 +0500

    Adding Invoice Form

--- erp/src/app/components/dashboard/invoices/invoices.component.ts
+++ erp/src/app/components/dashboard/invoices/invoices.component.ts
@@ -9,4 +9,10 @@ export class InvoicesComponent {
   colors = ['color1', 'color2', 'color3', 'color4'];
   protected invoices = invoiceInfo;
   protected invoiceInformation = tableInfor;
+  openForm = false;
+
+  invoiceForm() {
+    this.openForm = true;
+    console.log('CLICK');
+  }
 }
