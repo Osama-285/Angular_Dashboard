cb0c31b379443a53c4732cfc790aee2162f0526b
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-02-06 23:22:46 +0500

    invoice form

--- erp/src/app/components/dashboard/invoices/invoices.component.ts
+++ erp/src/app/components/dashboard/invoices/invoices.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { invoiceInfo, tableInfor } from 'src/app/constants/invoices.constants';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-invoices',
   templateUrl: './invoices.component.html',
@@ -10,10 +16,22 @@ export class InvoicesComponent {
   protected invoices = invoiceInfo;
   protected invoiceInformation = tableInfor;
   openForm = false;
+  protected invoiceFormData!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.invoiceFormData = this.fb.group({
+      client: ['', Validators.required],
+      invoiceNumber: ['', Validators.required],
+      amount: ['', Validators.required, Validators.pattern('^[0-9]*$')],
+      description: ['', Validators.required],
+      paymentStatus: [false, Validators.required],
+    });
+  }
 
   invoiceForm() {
     this.openForm = true;
-    console.log('CLICK');
   }
 
   closeModal() {
