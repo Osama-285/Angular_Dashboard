4824ae0ffd227b26596798ebf7f4cc8b03bb3362
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-02-10 22:32:03 +0500

    Form Validation

--- erp/src/app/components/dashboard/inventory/inventory.component.ts
+++ erp/src/app/components/dashboard/inventory/inventory.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { productInfo } from 'src/app/constants/inventory.constants';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
@@ -10,6 +11,20 @@ export class InventoryComponent {
   openForm: boolean = false;
   hoveredProduct: Object = {};
   products = productInfo;
+  protected productAddForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.productAddForm = this.fb.group({
+      code: ['', Validators.required],
+      brand: ['', Validators.required],
+      desc: ['', Validators.required],
+      categories: ['', Validators.required],
+      stock: ['', Validators.required],
+      sales: ['', Validators.required],
+    });
+  }
   onOptionSelected(): void {
     // console.log('Selected Option:', this.selectedOption);
   }
